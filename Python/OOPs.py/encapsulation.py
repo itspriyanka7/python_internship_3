04fd73483c946c208086a3a07ce153abb774658c
itspriyanka7 <desalepriyanka707@gmail.com>
2025-07-16 12:15:33 +0530

    Day 7 Assignments

---
+++ Python/OOPs.py/encapsulation.py
@@ -0,0 +1,14 @@
+class akshu:
+    def __init__(self,brand,model):
+        self.__brand = brand
+        self.model = model
+    def ishan(self):
+        print(f"My brand name is {self.__brand} and My model {self.model}")
+
+
+
+    
+
+
+
+
